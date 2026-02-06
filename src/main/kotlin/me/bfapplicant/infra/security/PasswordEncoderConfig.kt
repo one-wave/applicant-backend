a4b8f97d723f2d5990d0b6d6d392849e29d4f247
nampaca123 <nampaca123@gmail.com>
2026-02-06 23:05:30 +0900

    260206 jobpostop3

---
+++ src/main/kotlin/me/bfapplicant/infra/security/PasswordEncoderConfig.kt
@@ -0,0 +1,13 @@
+package me.bfapplicant.infra.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class PasswordEncoderConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+}
