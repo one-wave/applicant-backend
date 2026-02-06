9056d58875e6554657ec77ff252c03d09c24d4d1
nampaca123 <nampaca123@gmail.com>
2026-02-06 23:32:50 +0900

    260206 jobpostoppage2

--- src/main/kotlin/me/bfapplicant/feature/jobSearch/service/JobSearchService.kt
+++ src/main/kotlin/me/bfapplicant/feature/jobSearch/service/JobSearchService.kt
@@ -15,8 +15,10 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class JobSearchService(
     private val jobPostQueryRepository: JobPostQueryRepository,
     private val jobPostRepository: JobPostRepository,
